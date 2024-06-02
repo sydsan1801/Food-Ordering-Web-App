3bdf4374b68822e92cbe7c84f192a14058e036ae
sydsan1801 <sydsan1801@gmail.com>
2024-06-02 16:03:55 +0530

    second commit

--- server/controller/ImageUpload.js
+++ server/controller/ImageUpload.js
@@ -1,8 +1,8 @@
 const cloudinary=require('cloudinary');
 cloudinary.config({
-    cloud_name:"dlvplsrqv",
-    api_key:"556743134463841",
-    api_secret:"u6ETvbZBf0a6Xcb6YY6BvCRDRj4"
+    cloud_name:process.env.cloud_name_API,
+    api_key:process.env.api_key_API,
+    api_secret:process.env.api_secret_API,
 });
 
 const imageUploadController=async(req,res)=>{
